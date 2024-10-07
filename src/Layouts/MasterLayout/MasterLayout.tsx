56247e5d5f6ed51823c9f427a442c7a139122356
osama elgendy <osama elgendy.com>
2024-10-07 18:13:50 +0300

    ui-slider

--- src/Layouts/MasterLayout/MasterLayout.tsx
+++ src/Layouts/MasterLayout/MasterLayout.tsx
@@ -1,8 +1,13 @@
+
 import { Outlet } from "react-router-dom";
+import SideBar from "../../Pages/sideBar/SideBar";
 
 export default function MasterLayout() {
   return (
     <>
+    <div>
+<SideBar/>
+    </div>
       <Outlet />
     </>
   );
