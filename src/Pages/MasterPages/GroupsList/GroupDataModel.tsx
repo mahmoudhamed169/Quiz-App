283c8d06f4250f40642310e3f2c75ee3b6440ac5
mahmoudhamed169 <mh4190207@gmail.com>
2024-10-10 21:32:41 +0300

    add new group

--- src/Pages/MasterPages/GroupsList/GroupDataModel.tsx
+++ src/Pages/MasterPages/GroupsList/GroupDataModel.tsx
@@ -1,73 +1,161 @@
-import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import { useState } from "react";
+import { Button, Modal } from "flowbite-react";
+import { useEffect, useState } from "react";
 import CustomBtn from "../../../Components/MasterShared/CustomBtn/CustomBtn";
+import { Student } from "../../../InterFaces/Interfaces";
+// import { apiClient } from "../../../Apis/EndPoints";
+import { AxiosError } from "axios";
+import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { Loader } from "lucide-react";
+import { apiClient } from "./../../../Apis/EndPoints";
 
 export default function GroupDataModel() {
-  let [isOpen, setIsOpen] = useState(false);
+  const [allStudent, setAllStudent] = useState<Student[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [openModal, setOpenModal] = useState(false);
+
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    setFocus,
+    reset,
+  } = useForm<{ name: string; students: string[] }>();
+
+  const getAllStudent = async () => {
+    setLoading(true);
+    try {
+      const response = await apiClient.get<Student[]>("student");
+      setAllStudent(response.data);
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      toast.error(axiosError.response?.data?.message || "An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    reset();
+  };
+
+  useEffect(() => {
+    getAllStudent();
+  }, []);
+
+  useEffect(() => {
+    setFocus("name");
+  }, []);
+
+  const onSubmit = async (data: { name: string; students: string[] }) => {
+    const toastId = toast.loading("Processing...");
+
+    try {
+      const response = await apiClient.post("/group", data);
+      console.log(data);
+      console.log(response);
+
+      toast.success("Group created successfully!", {
+        id: toastId,
+      });
+
+      handleCloseModal();
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      toast.error(axiosError.response?.data?.message || "An error occurred", {
+        id: toastId,
+      });
+    }
+  };
 
   return (
     <>
-      <CustomBtn text="Add Group" onClick={() => setIsOpen(true)} />
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50"
-      >
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <CustomBtn text="Add Group" onClick={() => setOpenModal(true)} />
+      <Modal show={openModal} onClose={handleCloseModal}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex justify-between border-b items-center ">
+            <h5 className="font-bold text-xl leading-6 p-5">
+              Set up a new Group
+            </h5>
+            <div className="flex">
+              <button
+                type="submit"
+                className="text-2xl font-extrabold w-[80px] h-full border-l py-5"
+                disabled={isSubmitting || loading}
+              >
+                {isSubmitting ? (
+                  <Loader className="loader-spin m-auto " />
+                ) : (
+                  "\u2713"
+                )}
+              </button>
+              <button
+                type="button"
+                className="text-2xl font-extrabold border-l py-5 w-[80px]"
+                onClick={handleCloseModal}
+              >
+                {"\u2717"}
+              </button>
+            </div>
+          </div>
+          <Modal.Body className="overflow-hidden">
+            <div className="w-full relative mt-4 flex flex-col gap-8 ">
+              <div className="flex flex-col gap-1">
+                <div className="relative w-full">
+                  <input
+                    type="text"
+                    className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-lg border border-slate-400 rounded-md pr-3 pl-[7.3rem] py-2 focus:border-[#C5D86D] focus:outline-none focus:ring-1 focus:ring-[#C5D86D]"
+                    placeholder="Enter Group Name"
+                    {...register("name", { required: "Name is required" })}
+                  />
+                  <label className="font-bold absolute left-[0.9px] top-[0.8px] bottom-[0.8px] text-black bg-[#FFEDDF] py-3 px-3 border-e-0 flex items-center justify-center rounded-lg border border-transparent text-center text-sm transition-all shadow-sm">
+                    Group Name
+                  </label>
+                </div>
+                {errors.name && (
+                  <span className="text-red-600 ms-1">
+                    {errors.name.message}
+                  </span>
+                )}
+              </div>
 
-        <div className="fixed inset-0 flex items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12 rounded-lg transition-transform transform-gpu translate-y-0 animate-slide-in">
-            <div className="flex justify-between border-b items-center ">
-              <h5 className="font-bold text-xl leading-6 p-5">
-                Set up a new Group
-              </h5>
-              <div className="flex">
-                <button className="text-2xl font-extrabold w-[80px] h-full border-l py-5">
-                  {"\u2713"}
-                </button>
-                <button className="text-2xl font-extrabold border-l py-5 w-[80px]">
-                  {"\u2717"}
-                </button>
+              <div>
+                <div className="border w-full border-slate-400 rounded-lg">
+                  <label className="font-bold text-black bg-[#FFEDDF] py-3 px-3 border-e-0 flex items-center justify-center rounded-lg border border-transparent text-center text-sm transition-all shadow-sm">
+                    Select Students List
+                  </label>
+                  <select
+                    multiple
+                    {...register("students", {
+                      required: "At least one student must be selected",
+                    })}
+                    className="appearance-none w-full bg-transparent border-none focus:outline-none focus:ring-1 focus:ring-[#C5D86D] focus:border-transparent"
+                  >
+                    <option value="" disabled>
+                      Select Students List
+                    </option>
+                    {allStudent.map((student) => (
+                      <option
+                        value={student._id}
+                        key={student._id}
+                        className="text-slate-700 hover:bg-[#C5D86D] hover:text-white"
+                      >
+                        {`${student.first_name} ${student.last_name}`}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                {errors.students && (
+                  <span className="text-red-600 ms-1">
+                    {errors.students.message}
+                  </span>
+                )}
               </div>
             </div>
-
-            <p>This will permanently deactivate your account.</p>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
-            <div className="flex gap-4">
-              <button
-                onClick={() => setIsOpen(false)}
-                className="bg-gray-300 px-4 py-2 rounded"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={() => setIsOpen(false)}
-                className="bg-red-500 text-white px-4 py-2 rounded"
-              >
-                Deactivate
-              </button>
-            </div>
-          </DialogPanel>
-        </div>
-      </Dialog>
-
-      <style jsx>{`
-        .animate-slide-in {
-          opacity: 0;
-          transform: translateY(-20px);
-          animation: slideIn 0.3s forwards;
-        }
-
-        @keyframes slideIn {
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-      `}</style>
+          </Modal.Body>
+        </form>
+      </Modal>
     </>
   );
 }
